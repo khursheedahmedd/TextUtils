b87f26a3e908f7d4120043586daa8d986590f308
Pro-Programmer38 <khursheedahmed786k@gmail.com>
2023-10-01 01:56:29 +0500

    Update TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,38 +1,38 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
 export default function TextForm(props) {
- const handleUppercaseClick=()=>{
+    const handleUppercaseClick = () => {
         //console.log("Uppercase was clicked" + text);
         let newText = text.toUpperCase();
         setText(newText);
-        props.showAlert("Converted to uppercase","success");
+        props.showAlert("Converted to uppercase", "success");
     }
- const handleLowercasecaseClick=()=>{
+    const handleLowercasecaseClick = () => {
         //console.log("Uppercase was clicked" + text);
         let newText = text.toLowerCase();
         setText(newText);
-        props.showAlert("Converted to lowercase","success");
+        props.showAlert("Converted to lowercase", "success");
     }
- const handleClearTextClick=()=>{
+    const handleClearTextClick = () => {
         //console.log("Uppercase was clicked" + text);
         let newText = '';
         setText(newText);
-        props.showAlert("Text has been cleared","success");
+        props.showAlert("Text has been cleared", "success");
     }
- const handleOnChange=(event)=>{
+    const handleOnChange = (event) => {
         //console.log("OnChange");
         setText(event.target.value);
     }
-    const handleCopy=()=>{
+    const handleCopy = () => {
         let text = document.getElementById("mybox");
         text.select();
         navigator.clipboard.writeText(text.value);
-        props.showAlert("Text has been copied","success");
+        props.showAlert("Text has been copied", "success");
     }
-    const handleExtraSpaces=()=>{
-        let newText=text.split(/[ ]+/);
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[ ]+/);
         setText(newText.join(" "));
-        props.showAlert("Extra spaces has been removed","success")
+        props.showAlert("Extra spaces has been removed", "success")
     }
     // const handleTextAreaColor=()=>{
     //     if(props.mode === 'dark'){
@@ -44,34 +44,34 @@ export default function TextForm(props) {
     //         document.getElementById('mybox').style.color='black';
     //     }
     // }
-     const [text, setText] = useState('');
+    const [text, setText] = useState('');
     return (
         <>
-        <div className="Container my-6" style={{color: props.mode === 'dark' ? 'white':'black'}}>
-            <h3>{props.heading }</h3>
-            <div className="mb-3">
-                <textarea
-                    className="form-control"
-                    value={text}
-                    onChange={handleOnChange}
-                    style={{backgroundColor: props.mode === 'dark' ? '#6E717423':'white', color: props.mode === 'dark' ? 'white':'black'}}
-                    id="mybox"
-                    rows={8}
+            <div className="Container my-6" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
+                <h3>{props.heading}</h3>
+                <div className="mb-3">
+                    <textarea
+                        className="form-control"
+                        value={text}
+                        onChange={handleOnChange}
+                        style={{ backgroundColor: props.mode === 'dark' ? '#90959A23' : '#AAABAB23', color: props.mode === 'dark' ? 'white' : 'black' }}
+                        id="mybox"
+                        rows={8}
                     ></textarea>
+                </div>
+                <button type="button" className="btn btn-outline-primary  mx-1 my-1" onClick={handleUppercaseClick}>Convert to uppercase</button>
+                <button type="button" className="btn btn-outline-primary  mx-1 my-1" onClick={handleLowercasecaseClick}>Convert to lowercase</button>
+                <button type="button" className="btn btn-outline-primary  mx-1 my-1" onClick={handleClearTextClick}>Clear Text</button>
+                <button type="button" className="btn btn-outline-primary  mx-1 my-1" onClick={handleCopy}>Copy Text</button>
+                <button type="button" className="btn btn-outline-primary  mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
             </div>
-            <button type="button" className="btn btn-outline-primary  mx-1 my-1" onClick={handleUppercaseClick}>Convert to uppercase</button>
-            <button type="button" className="btn btn-outline-primary  mx-1 my-1" onClick={handleLowercasecaseClick}>Convert to lowercase</button>
-            <button type="button" className="btn btn-outline-primary  mx-1 my-1" onClick={handleClearTextClick}>Clear Text</button>
-            <button type="button" className="btn btn-outline-primary  mx-1 my-1" onClick={handleCopy}>Copy Text</button>
-            <button type="button" className="btn btn-outline-primary  mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
-        </div>
-        <div className="container my-4" style={{color: props.mode === 'dark' ? 'white':'black'}}>
-            <h3>Your Text Summary</h3>
-            <p>{(text.split(" ").filter((element)=>{return element.length!==0}).length)} words and {text.length} characters</p>
-            <p>{0.008*text.split(" ").filter((element)=>{return element.length!==0}).length} minutes read time</p>
-            <h5><b>Preview</b></h5>
-            <p>{text.length>0?text:"Text preview"}</p>
-        </div>
+            <div className="container my-4" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
+                <h3>Your Text Summary</h3>
+                <p>{(text.split(" ").filter((element) => { return element.length !== 0 }).length)} words and {text.length} characters</p>
+                <p>{0.008 * text.split(" ").filter((element) => { return element.length !== 0 }).length} minutes read time</p>
+                <h5><b>Preview</b></h5>
+                <p>{text.length > 0 ? text : "Text preview"}</p>
+            </div>
         </>
     );
 }
