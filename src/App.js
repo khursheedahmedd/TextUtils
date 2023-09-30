0a44f06b8a9d0ecb69aa57df295be943204f7145
Pro-Programmer38 <khursheedahmed786k@gmail.com>
2023-10-01 01:55:17 +0500

    no commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import Alert from './components/Alert';
+import About from './components/About';
+// import { Switch } from 'react-router-dom';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Switch,
+  Link
+} from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState('light');//whether dark mode is enable or not
+
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#333538';
+      showAlert("Dark mode has been enabled", "success");
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Switch>
+            <Route exat path="/about">
+              <About />
+            </Route>
+            <Route exact path="/">
+              <TextForm heading="Enter text to analyze" mode={mode} showAlert={showAlert} />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,77 @@
+import React,{useState} from "react";
+
+export default function TextForm(props) {
+ const handleUppercaseClick=()=>{
+        //console.log("Uppercase was clicked" + text);
+        let newText = text.toUpperCase();
+        setText(newText);
+        props.showAlert("Converted to uppercase","success");
+    }
+ const handleLowercasecaseClick=()=>{
+        //console.log("Uppercase was clicked" + text);
+        let newText = text.toLowerCase();
+        setText(newText);
+        props.showAlert("Converted to lowercase","success");
+    }
+ const handleClearTextClick=()=>{
+        //console.log("Uppercase was clicked" + text);
+        let newText = '';
+        setText(newText);
+        props.showAlert("Text has been cleared","success");
+    }
+ const handleOnChange=(event)=>{
+        //console.log("OnChange");
+        setText(event.target.value);
+    }
+    const handleCopy=()=>{
+        let text = document.getElementById("mybox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+        props.showAlert("Text has been copied","success");
+    }
+    const handleExtraSpaces=()=>{
+        let newText=text.split(/[ ]+/);
+        setText(newText.join(" "));
+        props.showAlert("Extra spaces has been removed","success")
+    }
+    // const handleTextAreaColor=()=>{
+    //     if(props.mode === 'dark'){
+    //         document.getElementById('mybox').style.backgroundColor='#6C6F73';
+    //         document.getElementById('mybox').style.color='white';
+    //     }
+    //     else{
+    //         document.getElementById('mybox').style.backgroundColor='white';
+    //         document.getElementById('mybox').style.color='black';
+    //     }
+    // }
+     const [text, setText] = useState('');
+    return (
+        <>
+        <div className="Container my-6" style={{color: props.mode === 'dark' ? 'white':'black'}}>
+            <h3>{props.heading }</h3>
+            <div className="mb-3">
+                <textarea
+                    className="form-control"
+                    value={text}
+                    onChange={handleOnChange}
+                    style={{backgroundColor: props.mode === 'dark' ? '#6E717423':'white', color: props.mode === 'dark' ? 'white':'black'}}
+                    id="mybox"
+                    rows={8}
+                    ></textarea>
+            </div>
+            <button type="button" className="btn btn-outline-primary  mx-1 my-1" onClick={handleUppercaseClick}>Convert to uppercase</button>
+            <button type="button" className="btn btn-outline-primary  mx-1 my-1" onClick={handleLowercasecaseClick}>Convert to lowercase</button>
+            <button type="button" className="btn btn-outline-primary  mx-1 my-1" onClick={handleClearTextClick}>Clear Text</button>
+            <button type="button" className="btn btn-outline-primary  mx-1 my-1" onClick={handleCopy}>Copy Text</button>
+            <button type="button" className="btn btn-outline-primary  mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+        </div>
+        <div className="container my-4" style={{color: props.mode === 'dark' ? 'white':'black'}}>
+            <h3>Your Text Summary</h3>
+            <p>{(text.split(" ").filter((element)=>{return element.length!==0}).length)} words and {text.length} characters</p>
+            <p>{0.008*text.split(" ").filter((element)=>{return element.length!==0}).length} minutes read time</p>
+            <h5><b>Preview</b></h5>
+            <p>{text.length>0?text:"Text preview"}</p>
+        </div>
+        </>
+    );
+}
